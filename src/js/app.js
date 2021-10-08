43b462b8c6a58c44cfabebea6531154a80fae353
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-10-08 14:52:02 +0100

    Updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
 /*===== WEBSITE SCROLL REVEAL =====*/
+
+/*===== SAVE CART =====*/
+// Change Save heart Icon
+function addToSavedItems() {
+	const replaceHeartfill = document.getElementById("bi").innerHTML;
+
+	replaceHeartfill.replace(
+		<i class="bi bi-heart"></i>,
+		<i class="bi bi-heart-fill"></i>
+	);
+}
