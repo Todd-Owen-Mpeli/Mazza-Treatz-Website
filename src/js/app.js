ec248f86bace20182598ec637a19c5ab4ad06bcd
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-10-11 14:09:18 +0100

    Updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,3 @@
 /*===== WEBSITE SCROLL REVEAL =====*/
-
-/*===== SAVE CART =====*/
-// Change Save heart Icon
-function changeHeartIcon() {
-	const replaceHeartfill = document.getElementById("bi").innerHTML;
-
-	replaceHeartfill.replace(
-		<i class="bi bi-heart"></i>,
-		<i class="bi bi-heart-fill"></i>
-	);
-}
+// 	AOS CDN.js Reveal Animation
+AOS.init();
